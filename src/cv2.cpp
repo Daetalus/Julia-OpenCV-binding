2180cb946725e8251ef7272279ea79cca3d0a74d
Boxiang Sun <daetalusun@gmail.com>
2014-09-07 23:49:43 +0800

    remove the intermedia struct JMat, pass cv::Mat directly between Julia and C++ as opeque type

--- src/cv2.cpp
+++ src/cv2.cpp
@@ -1,31 +1,27 @@
 #include "cv2.h"
+// include iostream header file for test
+#include <iostream>
 
-#include <stdio.h>
-using namespace cv;
+using namespace std;
 
-JMat* imread(char *filename)
+Mat* imread(char *filename)
 {
     cv::Mat img = cv::imread(filename);
-    static JMat* tempMat = (struct JMat*)malloc(sizeof(struct JMat));
+    cv::Mat *image = new Mat();
+    img.copyTo(*image);
 
-    if( img.empty())
-    {
-         std::cout<<"Can't load image "<<std::endl;
-    }
-    else {
-        tempMat->cols = img.cols;
-        tempMat->rows = img.rows;
-        mat2JMat(img, tempMat);
-    }
-    return tempMat;
+	return image;
 }
 
-void imshow(char *winName, JMat* mat){
-    //hardcode here.
-    cv::Mat image;
-    image.create(mat->rows, mat->cols, CV_8UC3);
-    JMat2Mat(mat, image);
-    cv::imshow(winName, image);
+void imshow(char *winName, Mat* mat){
+	if (!mat->empty())
+	{
+		cv::imshow(winName, *mat);
+	}
+	else
+	{
+		cout << "Empty image" << endl;
+	}
 }
 
 void waitKey(int delay)
@@ -33,39 +29,3 @@ void waitKey(int delay)
     cv::waitKey(delay);
 }
 
-/* dst = src */
-void JMat2Mat(JMat *src, cv::Mat &dst)
-{
-    dst.create(src->rows, src->cols, CV_8UC3);
-    const uchar* sptr = src->data;
-
-    //data copy using memcpy function
-    memcpy(dst.data, sptr, sizeof(unsigned char)*src->rows*src->cols*3);
-    return;
-}
-
-void mat2JMat(cv::Mat &src, JMat *dst)
-{
-    const uchar* sptr = src.data;
-
-    dst->data = (uchar*)malloc(sizeof(unsigned char)*src.rows*src.cols*src.elemSize());
-    //data copy using memcpy function
-    memcpy(dst->data, sptr, sizeof(unsigned char)*src.rows*src.cols*src.elemSize());
-    return;
-}
-
-void copyTo(cv::Mat &src, cv::Mat &dst)
-{
-    dst.create(src.rows, src.cols, src.type());
-
-    if( src.rows > 0 && src.cols > 0 )
-    {
-        const uchar* sptr = src.data;
-        uchar* dptr = dst.data;
-
-        size_t len = src.rows*src.elemSize();
-        //data copy using memcpy function
-        memcpy(dst.data, sptr, sizeof(unsigned char)*src.rows*src.cols*src.elemSize());
-    }
-    return;
-}
--- src/cv2.h
+++ src/cv2.h
@@ -3,23 +3,11 @@
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <iostream>
 
+using namespace cv;
 
-typedef struct JMat
-{
-    //! the number of rows and columns or (-1, -1) when the matrix has more than 2 dimensions
-    int rows, cols;
-    //! pointer to the data
-    uchar* data;
-    //More
-}JMat;
-
-void JMat2Mat(JMat *src, cv::Mat &dst);
-void mat2JMat(cv::Mat &src, JMat *dst);
-
-extern "C" JMat* imread(char *filename);
-extern "C" void imshow(char *winName, struct JMat* jmat_c);
+extern "C" Mat* imread(char *filename);
+extern "C" void imshow(char *winName, Mat *mat);
 extern "C" void waitKey(int delay);
 
 #endif // CV2_H
