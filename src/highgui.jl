19b059b3db6b59e972a3c40ce2978d95902825e5
Daetalus <daetalusun@gmail.com>
2014-09-20 17:14:25 +0800

    Move the test platform from 32bit to 64bit, set the parameter data type manually for now

--- src/highgui.jl
+++ src/highgui.jl
@@ -7,6 +7,6 @@ function imshow(winName::String, mat::Mat)
 	ccall( (:imshow, "../libcv2"), Void, (Ptr{Uint8}, Ptr{Void}), bytestring(winName), mat.handle)
 end
 
-function waitKey(delay::Int32)
+function waitKey(delay::Int)
 	ccall( (:waitKey, "../libcv2"), Void, (Int32,), delay)
 end
